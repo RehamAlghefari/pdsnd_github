56034f7a579d3f502260859f875d493263264f0e
Reham alghefari <reham-al-gheffari@hotmail.com>
2022-12-19 14:51:13 +0300

    feat: Load data for a city & filters by month & day

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -62,29 +62,29 @@ def load_data(city, month, day):
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
-    """
+     """
+    df = pd.read_csv(CITY_DATA[city])
+
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.strftime("%A")
+    df['Start hour'] = df['Start Time'].dt.hour
+
+    if month != 'all':
+
+        months = [ 'january', 'fabruary', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1
+        df = df[df['month'] == month]
+
+
+    if day != 'all':
+       df = df[df['day_of_week'] == day.title()]
+
 
 
     return df
 
-
-def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
-
-    print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()
-
-    # display the most common month
-
-
-    # display the most common day of week
-
-
-    # display the most common start hour
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
 
 
 def station_stats(df):
